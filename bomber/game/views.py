c89beeebe45405f7ec3e9a3bad5a6f8e5926d12d
newtmagalhaes <aniltonmagalhaes@gmail.com>
2022-05-02 20:14:05 -0300

    Adicionando arquivos estáticos

--- bomber/game/views.py
+++ bomber/game/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def bomber(request:HttpRequest) -> HttpResponse:
+  return HttpResponse('Hello *bombastic* world!')
+
+def index(request:HttpRequest) -> HttpResponse:
+  return render(request, 'index.html')
