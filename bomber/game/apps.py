42bbe214e679dd19a19da7c5b4314e50bbdd804f
newtmagalhaes <aniltonmagalhaes@gmail.com>
2022-05-02 19:05:15 -0300

    Renomeando projeto e app

--- bomber/game/apps.py
+++ bomber/game/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TodosConfig(AppConfig):
+class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'todos'
+    name = 'game'
