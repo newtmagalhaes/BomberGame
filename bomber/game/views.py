8b6a7842bc039ec60dfbd518d81a29ddaa85ed9f
newtmagalhaes <aniltonmagalhaes@gmail.com>
2022-05-03 15:06:08 -0300

    Adicionando estilo com Bootstrap

--- bomber/game/views.py
+++ bomber/game/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+def index(request:HttpRequest) -> HttpResponse:
+  return render(request, 'game/index.html')
+
+
 def bomber(request:HttpRequest) -> HttpResponse:
   return HttpResponse('Hello *bombastic* world!')
-
-def index(request:HttpRequest) -> HttpResponse:
-  return render(request, 'index.html')
